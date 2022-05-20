566438e05d404ad1706ce7decbd69114d312b064
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-05-20 08:56:14 -0500

    Tema 4 avances

---
+++ src/views/Tema4.vue
@@ -0,0 +1,174 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.bg-tema4
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Diagnóstico
+    .tarjeta.bg-tg-4.p-5.mb-5
+      .row.p-lg-5
+        .col-xl-6
+          p(data-aos='fade')  El diagnóstico puede definirse como una herramienta que ayuda a conocer detalladamente el estado actual de una organización e identificar los problemas que impiden su progreso. Existen diferentes tipos de metodologías para aplicar diagnósticos empresariales, algunos se enfocan en la empresa, otros en los clientes, otros en el área financiera, área de marketing, procesos de producción, entre otros. Lo más importante es obtener una información de la situación real de la empresa que permita tomar buenas decisiones para su proyección y crecimiento.
+    #t_4_1.titulo-segundo.color-acento-contenido
+      h2 4.1  Concepto de diagnóstico de clientes
+    .row.mb-3.justify-content-center
+      .col-lg-9.mb-3
+        .row
+          .col-lg-8.mb-3
+            p Entendiendo el diagnóstico como una herramienta empresarial que ayuda a conocer el estado actual de un proceso, entonces es posible decir que el diagnóstico de clientes es la obtención de todos los datos relacionados con el comportamiento de los clientes y su relación con la empresa en todas sus áreas, mercadeo, comunicación, ventas, servicio, entre otras. Es el conjunto de herramientas que ayuda a comprender el estado de las relaciones entre cliente-empresa-cliente.
+          .col-lg-4.mb-3.mb-lg-0: img(src='@/assets/curso/temas/94.jpg')
+          .col-lg-12
+            ul.lista-ul--color
+              li.mb-2
+                i.fas.fa-dot-circle.fa-xs
+                | Cuenta con la participación de todos los funcionarios de la empresa.
+              li.mb-2
+                i.fas.fa-dot-circle.fa-xs
+                | Compromiso del talento humano para identificar las fallas y proponer soluciones en la relación con el cliente.
+              li.mb-2
+                i.fas.fa-dot-circle.fa-xs
+                | Se identifica el problema de forma clara.
+              li.mb-2
+                i.fas.fa-dot-circle.fa-xs
+                | Debe ser un punto de partida para formular soluciones.
+              li.mb-2
+                i.fas.fa-dot-circle.fa-xs
+                | Es el inicio de todo proceso de planeación, en este caso, para diseñar estrategias con el cliente.
+      .col-lg-3: img(src='@/assets/curso/temas/95.jpg')
+
+    .d-flex.align-items-start.justify-content-center.mb-5
+        img(src="@/assets/curso/temas/dots-3.svg" style="width: 84px").me-3.d-lg-block.d-none
+        p.mb-0 En una empresa existen diferentes causas que llevan a la aplicación de un diagnóstico, tales como medir el nivel de satisfacción del cliente, la antigüedad de la relación cliente-empresa, la frecuencia de compra, niveles de atención, etc. De forma paralela, el diagnóstico le brinda a la empresa varios beneficios, como son:
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figure.mb-5
+          img(src='@/assets/curso/temas/96.svg', alt='Texto que describa la imagen')
+    Separador
+    #t_4_2.titulo-segundo.color-acento-contenido
+      h2 4.2  Tipos de diagnósticos
+    p.mb-5 De acuerdo con la finalidad del diagnóstico y los resultados esperados, existen diferentes tipos de diagnóstico que la empresa puede implementar con el fin de conocer aspectos de los clientes.
+    .bg-gris-separador
+      SlyderE.botones-slyder(indicadores="derecha")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h6  Integral
+            p Son un conjunto de variables que llevan a conocer las oportunidades, debilidades, fortalezas y las amenazas de la relación con el cliente.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/97.jpg', alt='Texto que describa la imagen')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h6 Específico
+            p Permite identificar el estado actual de una variable específica del cliente, por ejemplo: “Nivel de fidelización del cliente con respecto a un producto de la empresa”. Entre el diagnóstico específico, se encuentra: cultural, financiero, mercadeo, consumidor, etc.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/98.jpg', alt='Texto que describa la imagen')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h6 Cultural
+            p Permite reconocer los valores compartidos entre el cliente y la empresa. Aquí se interpretan valores como los sociales, espirituales y materiales, que definen el comportamiento y modos de pensar del cliente. Para este diagnóstico, se pueden usar entrevistas, reuniones, grupos focales, entre otras técnicas cualitativas.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/99.jpg', alt='Texto que describa la imagen')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h6 Consumidor – Cliente
+            p Permite profundizar en aspectos de cambios en los hábitos de consumo del cliente, frecuencia de compra, motivadores de decisión, detractores de compra, etc.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/100.jpg', alt='Texto que describa la imagen')
+    Separador
+    #t_4_3.titulo-segundo.color-acento-contenido
+      h2 4.3  Métodos y técnicas para el diagnóstico de clientes
+    .row.justify-content-center.mb-5
+      .col-lg-5.col-8: img.rounded-3(data-aos='zoom-in')(src='@/assets/curso/temas/101.jpg')
+      .col-lg-7
+        .cajon-editado
+          .bloque-texto-d.color-primario.p-4
+            .bloque-texto-d__texto.mb-2
+              i.fas.fa-quote-left
+              p.text-regular Existen diferentes métodos y técnicas para obtener la información del mercado que puede ser importante en la investigación. La selección del método de recolección de información depende del enfoque de la estrategia, las variables seleccionadas, la exactitud esperada, la zona de recolección y las capacidades del encuestador. Estas técnicas reúnen y ayudan a organizar la información de acuerdo con el objetivo trazado.
+              i.fas.fa-quote-right
+            .bloque-texto-d__autor 
+              .h6.mb-0 (Caro, 2019). 
+        p.mb-5 La recompra, la confianza, la satisfacción y las recomendaciones del producto hacia otras personas son variables importantes, que deben ser monitoreadas por la empresa. Para ello, es importante diseñar un diagnóstico enfocado al cliente (consumidor), que permita hacer monitoreo en las diferentes etapas del servicio. 
+    .row.justify-content-center.mb-5
+      .col-lg-3
+        .titulo.rounded-pill.color-primario.p-2.d-inline-block.mb-4.m-auto
+          .text-white: h4.mb-0.me-3 Paso 1: Selección de variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema4',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass">
+.bg-tema4
+  background-image: url(../assets/curso/temas/bg-tema3.png)
+  background-repeat: no-repeat
+  background-size: cover
+  background-position: start
+.bg-tg-4
+  background-image: url(../assets/curso/temas/bg-tg-4.png)
+  background-repeat: no-repeat
+  background-size: 100% 100%
+  background-position: center
+  @media (max-width: $bp-max-lg)
+    background-image: none
+    background-color: $color-neutral-2
+.bg-gris-separador
+  background-color: #d5dcdd
+  margin: 0 -3rem
+  padding: 3rem
+.botones-slyder
+  .slyder__btn
+    background-color: #f9e549
+.cajon-editado
+  .bloque-texto-d:before
+    opacity: .2 !important
+</style>
